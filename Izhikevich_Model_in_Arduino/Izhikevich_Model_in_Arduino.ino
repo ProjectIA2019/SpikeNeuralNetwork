d764dab6c7cf03ab684c78fc44f8aad7d5fa7e4a
Pietro Rignanese <pietro_rignanese@hotmail.it>
2019-01-19 14:38:43 +0100

    Add files via upload

--- Izhikevich_Model_in_Arduino/Izhikevich_Model_in_Arduino.ino
+++ Izhikevich_Model_in_Arduino/Izhikevich_Model_in_Arduino.ino
@@ -5,20 +5,124 @@ float t = 0; //Tempo
 float v = -70; //Potenziale membrana a riposo -70mV
 int u = -20; //Ripristino membrana -20mV
 
+int BOTTON = 2; //Bottone settaffio modalità di spike
+int mod = 0;
+
+int buzzerPIN = 4; //Buzzer per identificare lo spike
+
 float a = 0.02; //Scala temporale di u
 float b = 0.2; //Sensibilità di recupero
 int c = -65; //Valore di ripristino dopo il picco v
-int d = 6; //Ripristino di u dopo il picco
+float d = 6; //Ripristino di u dopo il picco
 
 String OutputStr = ""; //Valori di output da accumulare per essere lettu sull'oscilloscopio
 
 void setup()
 {
   Serial.begin(234000); // Inizializzazione porta seriale a 234000
+
+  //Settaggio bottone e modalità spike
+  pinMode(BOTTON,INPUT);
+  mod = 0;
+
+  pinMode(buzzerPIN, OUTPUT);
 }
 
 void loop()
-{    
+{
+
+//---------------SPIKE MODE--------------------------------  
+  //Controllo sul bottone per vedere la modalità attivata
+  if(digitalRead(BOTTON)==HIGH)
+  {
+    mod += 1;
+    if(mod > 9)
+    {
+      mod = 0;
+    }
+  }
+  if(mod == 0)
+  {
+    Serial.println("Tonic Spiking");
+    a = 0.02; //Scala temporale di u
+    b = 0.2; //Sensibilità di recupero
+    c = -65; //Valore di ripristino dopo il picco v
+    d = 6; //Ripristino di u dopo il picco
+  }
+  if(mod == 1)
+  {
+    Serial.println("Phasic Spiking");
+    a = 0.02; //Scala temporale di u
+    b = 0.25; //Sensibilità di recupero
+    c = -65; //Valore di ripristino dopo il picco v
+    d = 6; //Ripristino di u dopo il picco
+  }
+  if(mod == 2)
+  {
+    Serial.println("Tonic Bursting");
+    a = 0.02; //Scala temporale di u
+    b = 0.2; //Sensibilità di recupero
+    c = -50; //Valore di ripristino dopo il picco v
+    d = 2; //Ripristino di u dopo il picco
+  }
+  if(mod == 3)
+  {
+    Serial.println("Phasic Bursting");
+    a = 0.02; //Scala temporale di u
+    b = 0.25; //Sensibilità di recupero
+    c = -55; //Valore di ripristino dopo il picco v
+    d = 0.05; //Ripristino di u dopo il picco
+  }
+  if(mod == 4)
+  {
+    Serial.println("Mixed Mode");
+    a = 0.02; //Scala temporale di u
+    b = 0.2; //Sensibilità di recupero
+    c = -55; //Valore di ripristino dopo il picco v
+    d = 6; //Ripristino di u dopo il picco
+  }
+  if(mod == 5)
+  {
+    Serial.println("Spike Frequency Adaption");
+    a = 0.01; //Scala temporale di u
+    b = 0.2; //Sensibilità di recupero
+    c = -65; //Valore di ripristino dopo il picco v
+    d = 8; //Ripristino di u dopo il picco
+  }
+  if(mod == 6)
+  {
+    Serial.println("Class 1");
+    a = 0.02; //Scala temporale di u
+    b = - 0.1; //Sensibilità di recupero
+    c = -55; //Valore di ripristino dopo il picco v
+    d = 6; //Ripristino di u dopo il picco
+  }
+  if(mod == 7)
+  {
+    Serial.println("Class 2");
+    a = 0.2; //Scala temporale di u
+    b = 0.26; //Sensibilità di recupero
+    c = -65; //Valore di ripristino dopo il picco v
+    d = 0; //Ripristino di u dopo il picco
+  }
+  if(mod == 8)
+  {
+    Serial.println("Spike Latency");
+    a = 0.02; //Scala temporale di u
+    b = 0.2; //Sensibilità di recupero
+    c = -65; //Valore di ripristino dopo il picco v
+    d = 6; //Ripristino di u dopo il picco
+  }
+  if(mod == 9)
+  {
+    Serial.println("Resonator");
+    a = 0.1; //Scala temporale di u
+    b = 0.26; //Sensibilità di recupero
+    c = -60; //Valore di ripristino dopo il picco v
+    d = -1; //Ripristino di u dopo il picco
+  }
+//--------------------------------------------------------------
+  
   value = analogRead(potentiometerPin) - 512; // Lettura valore potenziometro
 
   //------------Modello Di Izhikevich---------------------------
@@ -34,6 +138,7 @@ void loop()
   //Controllo di picco per eventuale spike
   if(v > 30)
   {
+    tone(buzzerPIN, 1000, 500); //Il buzzer suona
     v = c;
     u += d;
   }
