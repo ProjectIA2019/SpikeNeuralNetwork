2bbf567de6d75c99729984d872b3f1fb46afe680
AndreaPolenta <andrea.polenta@outlook.it>
2019-01-28 17:43:46 +0100

    Update Sleepy_Neuron_Plot and fix sleepy_Neuron.pl

--- Program Prolog/SNN_Sleepy_Neuron.pl
+++ Program Prolog/SNN_Sleepy_Neuron.pl
@@ -6,10 +6,11 @@
  *
  */
 
-% ______________________________Izhikevich Model________________________
+% ______________________________Sleepy_Neuron_SSN_______________________
 %
-/*  Implementazione algoritmi base per la simulazione di una Spike Neural Network
- *                  utilizzando il modello di Izhikevich.
+/*  Implementazione di una Spike Neural Network utilizzando il modello di neurone dettato da E.Izhikevich.
+ *
+ *  I Neuroni in fase di riposo vengono considerati dormienti.
  *
  * TIPOLOGIE:
  *
@@ -36,30 +37,26 @@
  */
 
 
-spike(ts,0.02,0.2,-65,6,14).
+spike(ts,0.02,0.2,-65,6).
 
-spike(ps,0.02,0.25,-65,6,0.5).
+spike(ps,0.02,0.25,-65,6).
 
-spike(tb,0.02,0.2,-50,2,15).
+spike(tb,0.02,0.2,-50,2).
 
-spike(pb,0.02,0.25,-55,0.05,0.6).
+spike(pb,0.02,0.25,-55,0.05).
 
-spike(mm,0.02,0.2,-55,4,10).
+spike(mm,0.02,0.2,-55,4).
 
-spike(sfa,0.01,0.2,-65,8,30).
+spike(sfa,0.01,0.2,-65,8).
 
-spike(cuno,0.02,-0.1,-55,6,0).
+spike(cuno,0.02,-0.1,-55,6).
 
-spike(cdue,0.2,0.26,-65,0,0).
+spike(cdue,0.2,0.26,-65,0).
 
-spike(sl,0.02,0.2,-65,6,7).
+spike(sl,0.02,0.2,-65,6).
 
-spike(reson,0.1,0.26,-60,-1,0).
+spike(reson,0.1,0.26,-60,-1).
 
-%Spiking neural network
-s('Freya','Odino').
-s('Odino','Thor').
-s('Thor','Freya').
 %Info sugli spike
 spikehelp():-
     write("ELENCO FUNZIONI NEURONE:"), nl,
@@ -81,29 +78,37 @@ spikehelp():-
     write("16. snn(ib) -->  IB:  Inhibition-induce Bursting"), nl,
     nl.
 
-%Predicato per fallimento iniziale di start()
-nSpike(nil).
+%Spiking neural network
+s('Freya','Odino').
+s('Odino','Thor').
+s('Thor','Freya').
+
+%Inizializzazione lista dei neuroni presenti sulla SNN
+initNrnsList(InitNeuron,InitList,List):-
+    listControl(InitNeuron,InitList,NewList),
+    InitList \= NewList,
+    s(InitNeuron,PostNeuron),
+    initNrnsList(PostNeuron,NewList,List).
+initNrnsList(_,List,List).
 
 % Conrtrollo per il raggiungemento del picco
 % da parte del poteziale di membrana
-spikeControl(Neuron,Vf,Uf,C,D,Nlist,Spike) :-
+spikeControl(Neuron,Vf,Uf,C,D,Nlist) :-
     Vf >= 30,
-    assert(nSpike(Vf)),
     write('_____________________________________________________________'),nl,
     write('Registrato picco del neurone '),write(Neuron),nl,nl,
     write('Potenziale di membrana ==> '),write(Vf),write(' mV'),nl,
     write('Recupero di membrana   ==> '),write(Uf),write(' mV'),nl,
     write('_____________________________________________________________'),
     nl,nl,nl,nl,
-    sleep(2),
     NVf = C,
     NUf is Uf+D,
     listUpdate(Neuron-[NVf,NUf],Nlist,Newlist),
     s(Neuron,NeuronS),
-    start(Spike,NeuronS,Newlist,0.02).
-spikeControl(_,_,_,_,_,_,_).
+    start(NeuronS,Newlist).
+spikeControl(_,_,_,_,_,_).
 
-% Controllo sulla lista dei neuroni se il neurone considerato Ã¨ presente
+% Controllo sulla lista dei neuroni se il neurone considerato è presente
 listControl(Neuron,List,Nlist):-
     \+member(Neuron-_,List),
     append([Neuron-[-70,-20]],List,Nlist).
@@ -119,20 +124,57 @@ searchNeuron(Neuron,[Neuron-[NV,NU]|_],NV,NU).
 searchNeuron(Neuron,[_|T],NV,NU):-
     searchNeuron(Neuron,T,NV,NU).
 
-%Lanciatore
-snn(Spike,InitNeuron):-
-    start(Spike,InitNeuron,[],0.02).
+%Controllo per l'inserimento dei valori di Vf in un file
+controlFile(X,[_-[NV,_]|Tail]):-
+    write(X,NV),
+    write(X, " , "),
+    controlFile(X,Tail).
+controlFile(_,_).
 
-start(Spike,Neuron,Nlist,Tau):-
-    spike(Spike,A,B,C,D,I),
-    listControl(Neuron,Nlist,Newlist1),
-    searchNeuron(Neuron,Newlist1,Vi,Ui),
+% Funzione di sart che impone una corrente costante in ingresso ad un
+% certo neurone, eccitandolo e attivando tutti i processi della SNN
+start(Neuron,NrnsList):-
+    kSpike(Spike),
+    spike(Spike,A,B,C,D),
+    searchNeuron(Neuron,NrnsList,Vi,Ui),
+    initI(I),
+    tau(Tau),
     Vf is Vi+(0.04*Vi*Vi+5*Vi+140-Ui+I)*Tau,
     Uf is Ui+(A*(B*Vf-Ui))*Tau,
-    listUpdate(Neuron-[Vf,Uf],Newlist1,Newlist2),
-    spikeControl(Neuron,Vf,Uf,C,D,Newlist2,Spike),
-    start(Spike,'Freya',Newlist2,Tau).
+    listUpdate(Neuron-[Vf,Uf],NrnsList,Newlist),
+    initFile(File),
+    open(File,append,X),
+    sort(0,@<,Newlist,OrderList),
+    controlFile(X,OrderList),
+    nl(X),
+    close(X),
+    spikeControl(Neuron,Vf,Uf,C,D,Newlist),
+    initNeuron(InitNeuron),
+    start(InitNeuron,Newlist).
 
+
+%Lanciatore
+%
+%       Spike --> Tipologia di spike da lanciare
+%           I --> Corrente di sinapsi
+%         Tau --> Campionamento
+%  InitNeuron --> Neurone iniziale
+%        File --> File di destinazione su dove inserire i valori
+%
+
+snn(Spike,I,Tau,InitNeuron,File):-
+    retractall(tau(_)),
+    retractall(initI(_)),
+    retractall(kSpike(_)),
+    retractall(initNeuron(_)),
+    retractall(initFile(_)),
+    assert(initNeuron(InitNeuron)),
+    assert(initI(I)),
+    assert(kSpike(Spike)),
+    assert(tau(Tau)),
+    assert(initFile(File)),
+    initNrnsList(InitNeuron,[],List),
+    start(InitNeuron,List).
 
 
 
